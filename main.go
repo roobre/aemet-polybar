1b644537573ead9ae6d97699af7c545a35000748
Roberto Santalla <roobre@roobre.es>
2019-10-22 16:30:23 +0200

    Added flag support

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/xml"
 	"errors"
+	"flag"
 	"golang.org/x/text/encoding/charmap"
 	"io"
 	"log"
@@ -12,7 +13,11 @@ import (
 )
 
 func main() {
-	resp, _ := http.Get("https://www.aemet.es/xml/municipios_h/localidad_h_08019.xml")
+	city := flag.String("location", "08019", "City code from aemet URL (https://www.aemet.es/xml/municipios_h/localidad_h_08019.xml)")
+	hours := flag.Int("n", 2, "Number of hours in the future to output")
+	flag.Parse()
+
+	resp, _ := http.Get("https://www.aemet.es/xml/municipios_h/localidad_h_" + *city + ".xml")
 	dec := xml.NewDecoder(resp.Body)
 	dec.CharsetReader = func(charset string, input io.Reader) (reader io.Reader, e error) {
 		if charset != "ISO-8859-15" {
@@ -30,7 +35,7 @@ func main() {
 	forecast.Parse()
 
 	out := ""
-	for _, f := range forecast.NextHours(2) {
+	for _, f := range forecast.NextHours(*hours) {
 		out += f.String() + "  "
 	}
 
