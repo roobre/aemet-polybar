d1ef72a357d84a7f54e8892216932291ef07cc04
Roberto Santalla <roobre@roobre.es>
2021-03-28 14:12:22 +0200

    allow to specify a custom separator

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ func main() {
 	city := flag.String("location", "08019", "City code from aemet URL (https://www.aemet.es/xml/municipios_h/localidad_h_08019.xml)")
 	hours := flag.Int("n", 2, "Number of hours in the future to output")
 	labels := flag.String("labels", "", "List of hours and labels to show (e.g. \":13,19h:19\")")
+	separator := flag.String("separator", "  ", "Separate entries with this string.")
 	flag.Parse()
 
 	resp, err := http.Get("https://www.aemet.es/xml/municipios_h/localidad_h_" + *city + ".xml")
@@ -69,7 +70,7 @@ func main() {
 				continue
 			}
 
-			out += labelHour[0] + ": " + hourForecast.String() + "  "
+			out += labelHour[0] + ": " + hourForecast.String() + *separator
 		}
 	} else {
 		for _, f := range forecast.NextHours(*hours) {
